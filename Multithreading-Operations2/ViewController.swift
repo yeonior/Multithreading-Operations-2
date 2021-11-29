d672ed910e22e2de54f3705ef5a09945807b5d28
Rus <de.mr44@gmail.com>
2021-11-29 16:06:27 +0500

    Initial Commit

---
+++ Multithreading-Operations2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreading-Operations2
+//
+//  Created by ruslan on 29.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
