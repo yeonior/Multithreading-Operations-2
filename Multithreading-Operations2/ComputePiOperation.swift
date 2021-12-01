072a1ecc85efd75e4869be830414c8dff654dcff
Rus <de.mr44@gmail.com>
2021-12-01 22:53:42 +0500

    Added the estimation Pi using the Monte Carlo method

--- Multithreading-Operations2/ComputePiOperation.swift
+++ Multithreading-Operations2/ComputePiOperation.swift
@@ -21,7 +21,19 @@ final class ComputePiOperation: Operation {
         
         let startTime = Date()
         
-        
+        // estimating Pi using the Monte Carlo method
+        for _ in 0..._iterations {
+            
+            let (x, y) = (drand48() * 2 - 1, drand48() * 2 - 1)
+            let point = UIView(frame: CGRect(x: 3 * (x + 1) * 27, y: 3 * (y + 1) * 27, width: 1, height: 1))
+            if x * x + y * y <= 1 {
+                point.backgroundColor = UIColor.red
+                redPointsArray.append(point)
+            } else {
+                point.backgroundColor = UIColor.blue
+                bluePointsArray.append(point)
+            }
+        }
         
         print("Calculated in \(Date().timeIntervalSince(startTime)) seconds")
     }
